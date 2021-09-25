9418c1ad41a780e04473af259b075cfc94a2cd28
Timyr486786866745 <90512751+Timyr486786866745@users.noreply.github.com>
2021-09-25 16:13:39 +0300

    1.py

--- задание.py
+++ задание.py
@@ -5,7 +5,7 @@ class Critter:
     def status():
         print("Общее число фудболистов", Critter.total)
 
-    def __init__(self, name, hunger = 0, boredom = 0):
+    def __init__(self, name, hunger=0, boredom=0):
         self.__name = name
         Critter.total += 1
 
@@ -17,7 +17,6 @@ class Critter:
     def name(self):
         return self.__name
 
-   
     def name(self, new_name):
         if new_name == "":
             print("Имя фудболиста не может быть пустой строкой.")
@@ -25,53 +24,60 @@ class Critter:
             self.__name = new_name
             print("Имя успешно изменено.")
 
+
 def main():
     crit_name = input("Как вы назовете фудболиста?: ")
     crit = Critter(crit_name)
+
+
+def main2():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main3():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main4():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main5():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main6():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main7():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main8():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main9():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main10():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
+def main11():
+    crit_name = input("Как вы назовете фудболиста?: ")
+    crit = Critter(crit_name)
+
+
 main()
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-main()
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-main()
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-main()
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-main()
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-def main():
-    crit_name = input("Как вы назовете фудболиста?: ")
-    crit = Critter(crit_name)
-
-main()  
